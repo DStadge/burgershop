54f1e519f68fb93f56a9daef0987d94505e0fb48
Dirk Stadge <dirk_stadge@yahoo.de>
2023-06-01 09:46:21 +0200

    inital Commit

---
+++ src/main/java/de/iav/mucjave231burgershop/MucJave231BurgershopApplication.java
@@ -0,0 +1,13 @@
+package de.iav.mucjave231burgershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucJave231BurgershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MucJave231BurgershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/iav/mucjave231burgershop/MucJave231BurgershopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iav.mucjave231burgershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MucJave231BurgershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
